16d3a0b6377fc7cb65ce72e2b2ebdcd7c85af0d1
1krylova-stack <anastasia1991krylova@gmail.com>
2026-03-05 21:46:17 +0300

    Switch homepage copy to English and update US contact details

--- firefly/lp/sborka.php
+++ firefly/lp/sborka.php
@@ -1,5 +1,6 @@
 <?php
 $uid = get_the_ID();
+$is_en = true;
 
 /**
  * Anti-spam signed token (must match footer.php + mail.php)
@@ -17,7 +18,7 @@ $ff_token = hash_hmac('sha256', $ff_ts . '|' . $ff_ua, $ff_secret);
 		<div class="container">
 			<div class="titile"><?= get_field('title',$uid); ?></div>		
 			<div class="stats">					
-				<?php if(wpm_get_language() == 'en'):?>		
+				<?php if($is_en):?>		
 				<?php $table = get_field( 'sets_en',$uid ); ?>
 					<?php if ( $table ) {  ?>
 						<?php  foreach ( $table['body'] as $tr ) {  ?>
@@ -37,9 +38,9 @@ $ff_token = hash_hmac('sha256', $ff_ts . '|' . $ff_ua, $ff_secret);
 						}}}   ?>	
 				<?php endif; ?>				
 			</div>
-			<div <?php if(wpm_get_language() == 'en'): //Локализация?>id="action-eng"<?php endif; ?> class="action">
+			<div <?php if($is_en): //Локализация?>id="action-eng"<?php endif; ?> class="action">
 				<!-- <div class="text-action"><?= get_field('action',$uid); ?></div> -->
-				<div><a href="#" onclick="$(`#get_it [name='reason']`).val('Запрос прайс-листа (верхний баннер)'); return false;" data-toggle="modal" data-target="#get_it" class="y-but get-price"><?php if(wpm_get_language() == 'en'):?>GET A PRICE-LIST<?php else: ?>Получить прайс<?php endif; ?></a></div>
+				<div><a href="#" onclick="$(`#get_it [name='reason']`).val('Запрос прайс-листа (верхний баннер)'); return false;" data-toggle="modal" data-target="#get_it" class="y-but get-price"><?php if($is_en):?>GET A PRICE-LIST<?php else: ?>Получить прайс<?php endif; ?></a></div>
 			</div>
 		</div>
 	</div>
@@ -49,9 +50,9 @@ $ff_token = hash_hmac('sha256', $ff_ts . '|' . $ff_ua, $ff_secret);
 <section id="link_block" >
 	
 	<div class="container">
-		<div class="titile"><?php if(wpm_get_language() == 'en'): //Локализация?>OUR RANGE OF PRODUCTS<?php else: ?>Полный ассортимент нашей продукции<?php endif; ?></div>		
+		<div class="titile"><?php if($is_en): //Локализация?>OUR RANGE OF PRODUCTS<?php else: ?>Полный ассортимент нашей продукции<?php endif; ?></div>		
 		<div class="link_block">				
-			<a class="item" href="<?php if(wpm_get_language() == 'en'): //Локализация?>/en/category/produkciya/svetovozvrashhayushhaya-furnitura-dlya-proizvoditelej-odezhdy/<?php else: ?>/category/produkciya/svetovozvrashhayushhaya-furnitura-dlya-proizvoditelej-odezhdy/<?php endif; ?>">
+			<a class="item" href="<?php if($is_en): //Локализация?>/en/category/produkciya/svetovozvrashhayushhaya-furnitura-dlya-proizvoditelej-odezhdy/<?php else: ?>/category/produkciya/svetovozvrashhayushhaya-furnitura-dlya-proizvoditelej-odezhdy/<?php endif; ?>">
 				<span class="image first-image">
                     <img
                             src="<?php bloginfo('template_url')?>/imgs/l1.jpg" alt=""
@@ -59,12 +60,12 @@ $ff_token = hash_hmac('sha256', $ff_ts . '|' . $ff_ua, $ff_secret);
                             sizes="(max-width: 320px) 320px, (max-width: 480px) 480px, 604px"
                     >
                 </span>
-				<span class="name"><?php if(wpm_get_language() == 'en'): //Локализация?>GARMENT ACCESORIES<?php else: ?>Световозвращающая фурнитура<br>для производителей одежды<?php endif; ?></span>
+				<span class="name"><?php if($is_en): //Локализация?>GARMENT ACCESORIES<?php else: ?>Световозвращающая фурнитура<br>для производителей одежды<?php endif; ?></span>
 			</a>
-			<a class="item" href="<?php if(wpm_get_language() == 'en'): //Локализация?>/en/category/produkciya/katalog-svetovozvrashhayushhej-produkcii/<?php else: ?>/category/produkciya/katalog-svetovozvrashhayushhej-produkcii/<?php endif; ?>">
+			<a class="item" href="<?php if($is_en): //Локализация?>/en/category/produkciya/katalog-svetovozvrashhayushhej-produkcii/<?php else: ?>/category/produkciya/katalog-svetovozvrashhayushhej-produkcii/<?php endif; ?>">
 				<span class="image webpcheck" style="">
 				</span>
-				<span class="name"><?php if(wpm_get_language() == 'en'): //Локализация?>CATALOGUE OF OUR REFLECTIVE ACCESORIES<?php else: ?>Каталог<br>световозвращающей продукции<?php endif; ?></span>
+				<span class="name"><?php if($is_en): //Локализация?>CATALOGUE OF OUR REFLECTIVE ACCESORIES<?php else: ?>Каталог<br>световозвращающей продукции<?php endif; ?></span>
 			</a>
 		</div>
 	</div>
@@ -88,12 +89,12 @@ $ff_token = hash_hmac('sha256', $ff_ts . '|' . $ff_ua, $ff_secret);
 						<div class="a-at"><img class="lazy_loading" data-src="<?= get_field('at3-icon', $uid); ?>"><p><?= get_field('at3-text', $uid); ?></p></div>
 					</div>
 					<div>
-						<?php if(wpm_get_language() == 'en'): //Локализация?>
+						<?php if($is_en): //Локализация?>
 						<?php else: ?>
 						<?php foreach(acf_photo_gallery('cerf', get_the_ID()) as $cerf) { ?>
 						<a class="attachment_href" target="_blank" href="<?= $cerf['full_image_url']; ?>">
 							<?php if(stristr($cerf['title'], '[:ru]')):?>
-							<?php preg_match('~\[:'.wpm_get_language().'\](.+?)\[~', $cerf['title'], $matches);print_r($matches[1]); ?>
+							<?php preg_match('~\[:'.($is_en ? 'en' : wpm_get_language()).'\](.+?)\[~', $cerf['title'], $matches);print_r($matches[1]); ?>
 							<?php else: ?>
 							<?= $cerf['title']?>
 							<?php endif; ?>
@@ -103,7 +104,7 @@ $ff_token = hash_hmac('sha256', $ff_ts . '|' . $ff_ua, $ff_secret);
 					</div>
 				<?php } ?>
 			</div>
-			<?php if(wpm_get_language() == 'en'): //Локализация?>
+			<?php if($is_en): //Локализация?>
 				<div class="right-40">
 					<!--div class="info-img center"><img src="<?= get_field('info-img', $uid); ?>" ></div-->
 					<div class="round-banner">
@@ -179,7 +180,7 @@ $ff_token = hash_hmac('sha256', $ff_ts . '|' . $ff_ua, $ff_secret);
 
 							<div class="right">
 								<?php if(get_field('otziv-phone', $ben)) {?>
-									<p class="text text-right"><span>Тел:</span>  <?= get_field('otziv-phone', $ben); ?></p>
+									<p class="text text-right"><span>Phone:</span>  <?= get_field('otziv-phone', $ben); ?></p>
 								<?php } ?>
 								<p class="text text-right">  <?= get_field('otziv-spec', $ben); ?></p>
 								<p class="text text-right">  <?= get_field('otziv-fio', $ben); ?></p>
@@ -239,7 +240,7 @@ $ff_token = hash_hmac('sha256', $ff_ts . '|' . $ff_ua, $ff_secret);
 	</div>
 </section>
 
-<?php if(wpm_get_language() == 'en'): //Локализация?>
+<?php if($is_en): //Локализация?>
 <section id="skid">
 	<div class="bg" <?php if(0) { ?>style="background-image:url(<?= get_field('skid-img',$uid); ?>);"<?php } ?>>
 		<div class="container">
@@ -275,7 +276,7 @@ $ff_token = hash_hmac('sha256', $ff_ts . '|' . $ff_ua, $ff_secret);
 					<div class="cargo-img">
 						<img src="<?php bloginfo('template_url')?>/imgs/cargo.gif" alt="">
 					</div>
-					<a href="#" onclick="$(`#zvonok [name='reason']`).val('Получить прайс лист (блок скидки и доставка)'); return false;" data-toggle="modal" data-target="#zvonok" class="y-but action-but">Получить прайс-лист</a>
+					<a href="#" onclick="$(`#zvonok [name='reason']`).val('Получить прайс лист (блок скидки и доставка)'); return false;" data-toggle="modal" data-target="#zvonok" class="y-but action-but">GET A PRICE-LIST</a>
 				</div>
 			</div>
 		</div>
@@ -308,35 +309,28 @@ $ff_token = hash_hmac('sha256', $ff_ts . '|' . $ff_ua, $ff_secret);
 								<input type="text" name="website" value="" autocomplete="off" tabindex="-1"
 									   style="position:absolute;left:-9999px;height:0;width:0;opacity:0;">
 
-								<div class="ssf"><div class="left"><label><?php if(wpm_get_language() == 'en'): //Локализация?>NAME<?php else: ?>ИМЯ<?php endif; ?>:</label></div><div class="right"><input type="text" name="name" required /></div><div class="clearfix"></div></div>
+								<div class="ssf"><div class="left"><label><?php if($is_en): //Локализация?>NAME<?php else: ?>ИМЯ<?php endif; ?>:</label></div><div class="right"><input type="text" name="name" required /></div><div class="clearfix"></div></div>
 								<div class="ssf"><div class="left"><label>EMAIL:</label></div><div class="right"><input type="email" name="email" required /></div><div class="clearfix"></div></div>
-								<div class="ssf"><div class="left"><label><?php if(wpm_get_language() == 'en'): //Локализация?>PHONE NUMBER<?php else: ?>ТЕЛЕФОН<?php endif; ?>:</label></div><div class="right"><input type="text" name="phone" required /></div><div class="clearfix"></div></div>
-								<div class="ssf"><div class="left"><label><?php if(wpm_get_language() == 'en'): //Локализация?>QUESTION<?php else: ?>ВОПРОС<?php endif; ?>:</label></div><div class="right"><textarea name="question"></textarea></div><div class="clearfix"></div></div>
-								<div class="left personal-agree">	<label class="galka"> <input type="checkbox" required="" /> <?php if(wpm_get_language() == 'en'): //Локализация?>I give you permission to process<br>my personal data.<?php else: ?>Согласен на обработку<br/>персональных данных<?php endif; ?></label></div>
-								<div class="right submit-wrap">	<button type="submit" class="y-but y-but-invert "><?php if(wpm_get_language() == 'en'): //Локализация?>ASK A QUESTION<?php else: ?>Задать вопрос<?php endif; ?></button></div>
+								<div class="ssf"><div class="left"><label><?php if($is_en): //Локализация?>PHONE NUMBER<?php else: ?>ТЕЛЕФОН<?php endif; ?>:</label></div><div class="right"><input type="text" name="phone" required /></div><div class="clearfix"></div></div>
+								<div class="ssf"><div class="left"><label><?php if($is_en): //Локализация?>QUESTION<?php else: ?>ВОПРОС<?php endif; ?>:</label></div><div class="right"><textarea name="question"></textarea></div><div class="clearfix"></div></div>
+								<div class="left personal-agree">	<label class="galka"> <input type="checkbox" required="" /> <?php if($is_en): //Локализация?>I give you permission to process<br>my personal data.<?php else: ?>Согласен на обработку<br/>персональных данных<?php endif; ?></label></div>
+								<div class="right submit-wrap">	<button type="submit" class="y-but y-but-invert "><?php if($is_en): //Локализация?>ASK A QUESTION<?php else: ?>Задать вопрос<?php endif; ?></button></div>
 								<div class="clearfix"></div>
 							</form>
 						</div>
 					</div>
 					<div class="right-40">
 						<div class="cont-c">
-							<?php if(wpm_get_language() == 'en'): //Локализация?>
-								<div class="cont-t"> E-mail </div>
-								<div class="cont-d text"> <?= get_field('footer-email', $uid); ?> </div>
-							<?php else: ?>
-								<div class="cont-t">Телефон </div>
-								<div class="cont-d text"> <?= get_field('footer-phone', $uid); ?> </div>
-								<div class="cont-t"> E-mail </div>
-								<div class="cont-d text"> <?= get_field('footer-email', $uid); ?> </div>
-								<div class="cont-t"> Адрес </div>
-								<div class="cont-d text"> <?= get_field('footer-adress', $uid); ?> </div>
-								<div class="cont-t"> Мы в Whatsapp и Telegram </div>
+								<div class="cont-t">Phone</div>
+								<div class="cont-d text">+1-727-989-00-35</div>
+								<div class="cont-t">E-mail</div>
+								<div class="cont-d text">usa.reflective@gmail.com</div>
+								<div class="cont-t">We are on WhatsApp and Telegram</div>
 								<div class="cont-d text messengers">
-									<a href="https://wa.me/79006363775"><img src="<?php bloginfo('template_url')?>/imgs/wa-icon.svg" alt=""></a>
+									<a href="https://wa.me/17279890035"><img src="<?php bloginfo('template_url')?>/imgs/wa-icon.svg" alt=""></a>
 									<a href="https://t.me/svetlyachokk"><img src="<?php bloginfo('template_url')?>/imgs/tg-icon.svg" alt=""></a>
 								</div>
-							<?php endif; ?>
-						</div>
+							</div>
 					</div>
 					<div class="clearfix"></div>
 				</div>
