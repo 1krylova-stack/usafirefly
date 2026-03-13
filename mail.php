67d7891214b6a1297012312fc436c28e34ca2bba
1krylova-stack <anastasia1991krylova@gmail.com>
2026-03-13 23:32:46 +0300

    Disable CRM routing and unify lead email delivery

--- mail.php
+++ mail.php
@@ -64,11 +64,15 @@ if ($now - $last < 15) {
 file_put_contents($key, (string)$now);
 
 /* =========================
-   REFERER CHECK
+   REFERER CHECK (same host)
 ========================= */
 $ref = (string)($_SERVER['HTTP_REFERER'] ?? '');
-if ($ref !== '' && stripos($ref, 'svetlyachok.info') === false) {
-    fail(200, 'Bad referer');
+$host = (string)($_SERVER['HTTP_HOST'] ?? '');
+if ($ref !== '' && $host !== '') {
+    $refHost = (string)parse_url($ref, PHP_URL_HOST);
+    if ($refHost !== '' && strcasecmp($refHost, $host) !== 0) {
+        fail(200, 'Bad referer');
+    }
 }
 $page = ($ref !== '') ? strtok($ref, '?') : '';
 
@@ -137,60 +141,8 @@ try {
     // Reply-To на рабочий ящик + на email клиента (ниже добавим)
     $mail->addReplyTo('svet@svetlyachok.info', 'Светлячок');
 
-    // Всегда отправляем на эти
-    $mail->addAddress('svet@svetlyachok.info');
+    // Все заявки отправляем на один ящик (без маршрутизации)
     $mail->addAddress('miccollector01@yandex.ru');
-
-    // Ваша логика маршрутизации (оставляем)
-    $pages = [
-        'https://svetlyachok.info/shnury/',
-        'https://svetlyachok.info/shevrony/',
-        'https://svetlyachok.info/velkro-novogo-pokoleniya/',
-        'https://svetlyachok.info/shevrony-mirovogo-urovnya/',
-        'https://svetlyachok.info/termo-lenta-dlya-shvov/',
-        'https://svetlyachok.info/termometry-dlya-odezhdy/',
-        'svetlyachok.info/shnury/',
-        'svetlyachok.info/shevrony/',
-        'svetlyachok.info/velkro-novogo-pokoleniya/',
-        'svetlyachok.info/shevrony-mirovogo-urovnya/',
-        'svetlyachok.info/termo-lenta-dlya-shvov/',
-        'svetlyachok.info/termometry-dlya-odezhdy/',
-        'https://svetlyachok.info/shnury',
-        'https://svetlyachok.info/shevrony',
-        'https://svetlyachok.info/velkro-novogo-pokoleniya',
-        'https://svetlyachok.info/shevrony-mirovogo-urovnya',
-        'https://svetlyachok.info/termo-lenta-dlya-shvov',
-        'https://svetlyachok.info/termometry-dlya-odezhdy',
-        'svetlyachok.info/shnury',
-        'svetlyachok.info/shevrony',
-        'svetlyachok.info/velkro-novogo-pokoleniya',
-        'svetlyachok.info/shevrony-mirovogo-urovnya',
-        'svetlyachok.info/termo-lenta-dlya-shvov',
-        'svetlyachok.info/termometry-dlya-odezhdy',
-
-        'https://svetlyachok.info/lyuversy/',
-        'https://svetlyachok.info/nakonechniki-na-shnur/',
-        'https://svetlyachok.info/fiksatory-dlya-shnura/',
-        'https://svetlyachok.info/molnii-i-knopki-ukk/',
-        'svetlyachok.info/lyuversy/',
-        'svetlyachok.info/nakonechniki-na-shnur/',
-        'svetlyachok.info/fiksatory-dlya-shnura/',
-        'svetlyachok.info/molnii-i-knopki-ukk/',
-        'https://svetlyachok.info/lyuversy',
-        'https://svetlyachok.info/nakonechniki-na-shnur',
-        'https://svetlyachok.info/fiksatory-dlya-shnura',
-        'https://svetlyachok.info/molnii-i-knopki-ukk',
-        'svetlyachok.info/lyuversy',
-        'svetlyachok.info/nakonechniki-na-shnur',
-        'svetlyachok.info/fiksatory-dlya-shnura',
-        'svetlyachok.info/molnii-i-knopki-ukk'
-    ];
-
-    if (in_array($page, $pages, true)) {
-        $mail->addAddress('Grea1@yandex.ru');
-    } else {
-        $mail->addAddress('roman.parshukov@gmail.com');
-    }
 
     $mail->isHTML(true);
     $mail->Subject = 'Новая заявка';
@@ -222,17 +174,6 @@ try {
         ' | page=' . $page .
         ' | to=' . implode(',', array_column($mail->getToAddresses(), 0))
     );
-
-    // CRM (как было раньше)
-    $crmUrl = in_array($page, $pages, true)
-        ? "http://crm2.svetlyachok.info/index.php/common/createbysite?"
-        : "http://crm.svetlyachok.info/index.php/common/createbysite?";
-
-    $postForCrm = $_POST;
-    unset($postForCrm['website'], $postForCrm['ff_ts'], $postForCrm['ff_token']);
-
-    $ctx = stream_context_create(['http' => ['timeout' => 3]]);
-    @file_get_contents($crmUrl . http_build_query($postForCrm), false, $ctx);
 
     die(json_encode(['success' => true]));
 } catch (\Throwable $e) {
